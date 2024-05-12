02839caab211173d9cb099e04401332f7b5c85b6
smasse <smasse@student.42luxembourg.lu>
2024-05-12 06:11:13 +0200

    Added description

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='normino',
     version='0.1',
@@ -7,6 +10,8 @@ setup(
     install_requires=[
         'colorama',
     ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     entry_points={
         'console_scripts': [
             'normino=normino:main',
