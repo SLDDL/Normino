b5c33c5341373fb2917ee9772dc3bc602774ce0c
smasse <smasse@student.42luxembourg.lu>
2024-05-12 06:14:30 +0200

    increment vers

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='normino',
-    version='0.1',
+    version='0.2',
     py_modules=['normino'],
     install_requires=[
         'colorama',
